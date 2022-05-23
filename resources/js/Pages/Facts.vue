dbbe5d26d93d0da91f9f579a7026f156ed1f2d45
loacoco <66422809+loacoco@users.noreply.github.com>
2022-05-23 12:13:24 +0200

    consume API

---
+++ resources/js/Pages/Facts.vue
@@ -0,0 +1,10 @@
+<template>
+    <AppLayout title="Facts">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Facts
+            </h2>
+        </template>
+
+    </AppLayout>
+</template>
